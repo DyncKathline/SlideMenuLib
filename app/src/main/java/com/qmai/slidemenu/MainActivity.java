1032f31663a45a391c96fd4b4234495a090f775b
DyncKathline <1350624667@qq.com>
2018-11-07 18:10:43 +0800

    侧滑菜单和RecycleView完美解决冲突

---
+++ app/src/main/java/com/qmai/slidemenu/MainActivity.java
@@ -0,0 +1,41 @@
+package com.qmai.slidemenu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.qmai.slidemenu.view.SlidingMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String TAG = getClass().getSimpleName();
+    private SlidingMenu mSlidingMenu;
+    private RecyclerView mRecyclerView;
+    private SlideAdapter mAdapter;
+    private List<String> data = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mSlidingMenu = (SlidingMenu) findViewById(R.id.slidingMenu);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        initData();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager( this));
+        mRecyclerView.setAdapter(mAdapter = new SlideAdapter(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mAdapter.setData(data);
+    }
+
+    private void initData() {
+        for (int i = 0; i < 20; i++) {
+            data.add("测试数据" + i);
+        }
+    }
+}
---
+++ app/src/main/java/com/qmai/slidemenu/SlideAdapter.java
@@ -0,0 +1,84 @@
+package com.qmai.slidemenu;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlideAdapter extends RecyclerView.Adapter<SlideAdapter.MyViewHolder> {
+    private List<String> list_data = new ArrayList<>();
+    private Context mContext;
+
+    public SlideAdapter(Context context){
+        mContext = context;
+    }
+
+    @NonNull
+    @Override
+    public SlideAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        final View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.item_swipe, viewGroup, false);
+        SlideAdapter.MyViewHolder holder = new SlideAdapter.MyViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final SlideAdapter.MyViewHolder myViewHolder, int position) {
+        myViewHolder.tv_title.setText(list_data.get(position));
+//        myViewHolder.view_del.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                int n =  myViewHolder.getLayoutPosition();
+//                Log.e("---=","delete");
+//                removeData(n);
+//            }
+//        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return list_data.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+        TextView tv_title;
+        TextView view_del;
+        TextView view_edit;
+        TextView tv_top;
+        ImageView iv_avatar;
+//        SwipeMenuLayout swipeMenuLayout;
+        View contentView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tv_title = (TextView) itemView.findViewById(R.id.item_tv_title);
+//            tv_top = (TextView) itemView.findViewById(R.id.item_tv_top);
+//            contentView = itemView.findViewById(R.id.item_content);
+//
+//            view_del = (TextView) itemView.findViewById(R.id.item_tv_del);
+//            view_edit = (TextView) itemView.findViewById(R.id.item_tv_edit);
+//            swipeMenuLayout = (SwipeMenuLayout) itemView.findViewById(R.id.item_layout_swip);
+            iv_avatar = (ImageView) itemView.findViewById(R.id.item_avatar);
+        }
+    }
+
+    public void setData(List<String> list) {
+        if(list != null) {
+            list_data.clear();
+            list_data.addAll(list);
+            notifyDataSetChanged();
+        }
+    }
+
+    public void removeData(int position){
+        list_data.remove(position);
+        notifyItemRemoved(position);
+
+    }
+
+}

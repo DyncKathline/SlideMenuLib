f7b87b6fd3e81bbdcde71da31d2926edb1a3a961
DyncKathline <1350624667@qq.com>
2018-11-07 18:46:53 +0800

    增加一个滑动返回类，没有移动效果

--- app/src/main/java/com/qmai/slidemenu/MainActivity.java
+++ app/src/main/java/com/qmai/slidemenu/MainActivity.java
@@ -1,41 +1,27 @@
 package com.qmai.slidemenu;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-
-import com.qmai.slidemenulib.SlidingMenu;
-
-import java.util.ArrayList;
-import java.util.List;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
-    private final String TAG = getClass().getSimpleName();
-    private SlidingMenu mSlidingMenu;
-    private RecyclerView mRecyclerView;
-    private SlideAdapter mAdapter;
-    private List<String> data = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mSlidingMenu = (SlidingMenu) findViewById(R.id.slidingMenu);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        initData();
-        mRecyclerView.setLayoutManager(new LinearLayoutManager( this));
-        mRecyclerView.setAdapter(mAdapter = new SlideAdapter(this));
-        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
-        mAdapter.setData(data);
     }
 
-    private void initData() {
-        for (int i = 0; i < 20; i++) {
-            data.add("测试数据" + i);
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.slideMenu:
+                startActivity(new Intent(MainActivity.this, SlideMenuActivity.class));
+                break;
+            case R.id.slideBack:
+                startActivity(new Intent(MainActivity.this, SlideBackActivity.class));
+                break;
         }
     }
+
 }
---
+++ app/src/main/java/com/qmai/slidemenu/SlideMenuActivity.java
@@ -0,0 +1,40 @@
+package com.qmai.slidemenu;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.qmai.slidemenulib.SlidingMenu;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SlideMenuActivity extends AppCompatActivity {
+    private final String TAG = getClass().getSimpleName();
+    private SlidingMenu mSlidingMenu;
+    private RecyclerView mRecyclerView;
+    private SlideAdapter mAdapter;
+    private List<String> data = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide_menu);
+
+        mSlidingMenu = (SlidingMenu) findViewById(R.id.slidingMenu);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        initData();
+        mRecyclerView.setLayoutManager(new LinearLayoutManager( this));
+        mRecyclerView.setAdapter(mAdapter = new SlideAdapter(this));
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mAdapter.setData(data);
+    }
+
+    private void initData() {
+        for (int i = 0; i < 20; i++) {
+            data.add("测试数据" + i);
+        }
+    }
+}
